100912cdd7f3d305f82647ce59b5e507a4d4ecc3
Thanongsak N <thanongsak_n@cmu.ac.th>
2016-09-09 16:23:19 +0700

    eiei

--- src/cpe200/Course.java
+++ src/cpe200/Course.java
@@ -10,16 +10,27 @@ public class Course {
     }
 
     public Course(String n, String cid) {
-        // implement here
+        this.course_name = !n.equalsIgnoreCase("")?n:"TBA";;
+        this.course_id = isValidCourse_id(cid)?cid:"000000";
+        this.lecturer = "TBA";
+        this.max_students = 30;
+        this.no_students = 0;
     }
 
     public Course(String n, String cid, String l) {
-        // implement here
+        this.course_name = !n.equalsIgnoreCase("")?n:"TBA";;
+        this.course_id = isValidCourse_id(cid)?cid:"000000";
+        this.lecturer = !l.equalsIgnoreCase("")?l:"TBA";;
+        this.max_students = 30;
+        this.no_students = 0;
     }
 
     public Course(String n, String cid, String l, int max) {
         this.course_name = !n.equalsIgnoreCase("")?n:"TBA";
-        // implement the rest here
+        this.course_id = isValidCourse_id(cid)?cid:"000000";
+        this.lecturer = !l.equalsIgnoreCase("")?l:"TBA";
+        this.max_students = max;
+        this.no_students = 0;
     }
 
     public String getCourse_name() {
@@ -40,39 +51,47 @@ public class Course {
 
     // implement the other get and set methods here
     public String getLecturer() {
-        // implement here
-        return "lecturer";
+        return lecturer;
     }
 
     public void setLecturer(String lecturer) {
-        // implement here
+        this.lecturer = !lecturer.equalsIgnoreCase("")?lecturer:this.lecturer;
     }
 
     public int getMax_students() {
-        // implement here
-        return 0;
+        return max_students;
     }
 
     public void setMax_students(int max_students) {
-        // implement here
+        if(max_students>9){
+            this.max_students = max_students;
+        }
     }
 
     public int getNo_students() {
-        // implement here
-        return 0;
+        return no_students;
     }
 
     public void setNo_students(int no_students) {
-        // implement here
+        if(no_students >=0 && no_students<=max_students){
+            this.no_students = no_students;
+        }
     }
 
     @Override
     public String toString() {
-        String o = this.course_name + " ("
-                + this.course_id + "), Teacher: "
-                + this.lecturer + ", has ";
 
-        // implement the rest here
+        String quantity;
+        if(no_students == 0){
+            quantity = "NO student" + ", [maximum: "+max_students+"]";
+        }
+        else if(no_students == 1){
+            quantity = "ONE student" + ", [maximum: "+max_students+"]";
+        }
+        else{
+            quantity = no_students  + " students"+", [maximum: "+max_students+"]";
+        }
+        String o = this.course_name + " ("  + this.course_id + "), Teacher: "  + this.lecturer + ", has "+ quantity;
 
         return o;
     }
@@ -81,9 +100,7 @@ public class Course {
         Pattern p = Pattern.compile(idREGEX);
         Matcher m = p.matcher(id);
 
-        // implement the rest here
-
-        return true;
+        return m.matches();
     }
 
     // Regular expression for the Student ID pattern
--- src/cpe200/Student.java
+++ src/cpe200/Student.java
@@ -1,30 +1,129 @@
 package cpe200;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Student {
 
     public Student() {
-        // implement here
+        name = "John Doe";
+        studentid = "560610000";
+        yearofbirth = 1990;
+        status =  false;
     }
 
-    // implement all missing constructors here
+    public Student(String n, String sid) {
+        this.name = !n.equalsIgnoreCase("") ? n : "TBA";
 
-    // implement all get and set methods here
+        if(this.isValidStudent_id(sid)){
+            this.studentid = sid;
+        }else {
+            this.studentid = "560610000";
+        }
+        yearofbirth = 1990;
+        status =  false;
+    }
+    public Student(String n, String sid, int y) {
+        this.name = !n.equalsIgnoreCase("") ? n : "TBA";
+
+        if(this.isValidStudent_id(sid)){
+            this.studentid = sid;
+        }else{
+            this.studentid  = "560610000";
+        }
+        if(this.isValidYOB(y)) {
+            this.yearofbirth = y;
+        }else{
+            this.yearofbirth = 1990;
+        }
+        status =  false;
+    }
+    public Student(String n, String sid, int y, boolean stat) {
+        this.name = !n.equalsIgnoreCase("") ? n : "TBA";
+
+        if(this.isValidStudent_id(sid)){
+            this.studentid = sid;
+        }else{
+            this.studentid  = "560610000";
+        }
+        if(this.isValidYOB(y)) {
+            this.yearofbirth = y;
+        }else{
+            this.yearofbirth = 1990;
+        }
+        this.status = stat;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+    public void setName(String name) {
+        this.name = !name.equalsIgnoreCase("")?name:this.name;
+    }
+    public String getStudent_id(){
+        return this.studentid;
+    }
+    public void setStudent_id(String studentid) {
+        this.studentid = isValidStudent_id(studentid)?studentid:this.studentid;
+    }
+
+    public int getYearOfBirth() {
+        return this.yearofbirth;
+    }
+
+    public void setYearOfBirth(int yearofbirth) {
+        if(yearofbirth >= 1990){
+            this.yearofbirth = yearofbirth;
+        }
+    }
+
+    public boolean getStatus(){
+        return this.status;
+    }
+
+    public boolean isActive() {
+        if (status == true) {
+            return true;
+        }
+        return false;
+    }
 
     @Override
     public String toString() {
-        String o = "Something";
+        String n = "";
+        if(this.isActive()== true){
+            n = "ACTIVE";
+        }
+        else if(this.isActive()==false){
+            n = "INACTIVE";
+        }
+        String o = this.name + " ("+this.studentid+") "+"was born in "+this.yearofbirth+" is an "+n+" student.";
 
         return o;
     }
 
-    private boolean isValidStudent_id(String id) {
-        return true;
-    }
-
     private boolean isValidYOB(int yob) {
+        if(yob <1990){
+            return false;
+        }
         return true;
     }
 
-    // declare your attributes here
+    private String name;
+    private String studentid;
+    private int yearofbirth;
+    private boolean status;
+
+    private boolean isValidStudent_id(String id) {
+        Pattern p = Pattern.compile(idREGEX);
+        Matcher m = p.matcher(id);
+
+
+
+        return m.matches();
+    }
+
+    // Regular expression for the Student ID pattern
+    private static final String idREGEX = "^5([6-9]?)061([0-2]?)([0-9]{3})$";
 
 }
